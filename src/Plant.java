05f1339fab56243575bda5b5d4e50e0ff66195ad
Sofia Lavrekhina <sofi0702@icloud.com>
2025-09-24 17:12:20 +0100

    Plant class created

--- src/Plant.java
+++ src/Plant.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Plant {
+    //attributes
+    private String name;
+    private int moistureLevel;
+
+    //constructor
+    public Plant(String name, int level){
+        this.name = name;
+        this.moistureLevel = level;
+    }
+
+    //getters
+    public String getName(){
+        return this.name;
+    }
+    public int getMoistureLevel(){
+        return this.moistureLevel;
+    }
+    //setters
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setMoistureLevel(int level){
+        this.moistureLevel = moistureLevel;
+    }
+
 }
